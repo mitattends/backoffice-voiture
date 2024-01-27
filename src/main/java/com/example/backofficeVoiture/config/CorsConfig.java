786bd0225b115a0affb00d9945f89e1ec4ca9e56
01KingMaker <fabrakoto0987@gmail.com>
2024-01-27 18:26:40 +0300

    modify access permission

--- src/main/java/com/example/backofficeVoiture/config/CorsConfig.java
+++ src/main/java/com/example/backofficeVoiture/config/CorsConfig.java
@@ -1,20 +1,16 @@
 package com.example.backofficeVoiture.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-// c
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
     }
 }
