3160a48ef8a77a6810a96fdb773b169a68d32ba9
01KingMaker <fabrakoto0987@gmail.com>
2024-01-27 20:28:16 +0300

    modify access for deploying server

--- src/main/java/com/example/backofficeVoiture/BackofficeVoitureApplication.java
+++ src/main/java/com/example/backofficeVoiture/BackofficeVoitureApplication.java
@@ -3,7 +3,10 @@ package com.example.backofficeVoiture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 public class BackofficeVoitureApplication {
 
 	public static void main(String[] args) {
