9dc3cba6a3c7a717e2d5bdb1be14d670a9974aea
01KingMaker <fabrakoto0987@gmail.com>
2024-01-28 03:09:05 +0300

    acces cors

--- src/main/java/com/example/backofficeVoiture/BackofficeVoitureApplication.java
+++ src/main/java/com/example/backofficeVoiture/BackofficeVoitureApplication.java
@@ -2,15 +2,25 @@ package com.example.backofficeVoiture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-})
+@SpringBootApplication
 public class BackofficeVoitureApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackofficeVoitureApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
